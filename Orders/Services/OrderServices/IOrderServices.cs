db7e9d42c359d38779b3ec9955cd16385f75fa12
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2022-11-10 20:45:31 +0600

    CreateOrder, CreateOrderItemToOrder, GetOrders => OrderServices

--- Orders/Services/OrderServices/IOrderServices.cs
+++ Orders/Services/OrderServices/IOrderServices.cs
@@ -1,5 +1,6 @@
 ﻿using Orders.ViewModels.OrderItem;
 using Orders.ViewModels.Orders;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Orders.Services.OrderServices
@@ -9,7 +10,7 @@ namespace Orders.Services.OrderServices
         public Task<OrderViewModel> CreateOrder(OrderViewModel orders);
         public Task<OrderItemViewModel> CreateOrderItemToOrder(OrderItemViewModel orderItem);
         public Task<OrderItemViewModel> AddOrderItemToOrder(OrderItemViewModel orderItem);
-        public Task<OrderTableViewModel> GetOrders();
+        public Task<IEnumerable<OrderTableViewModel>> GetOrders();
         public Task<OrderTableViewModel> GetOrders(int id);
         public Task<bool> UpdateOrder(OrderUpdateViewModel orders);
         public Task<bool> UpdateOrderItem(OrderItemUpdateViewModel orderItem);
--- Orders/Services/OrderServices/OrderServices.cs
+++ Orders/Services/OrderServices/OrderServices.cs
@@ -1,6 +1,10 @@
-﻿using Orders.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Orders.Data;
+using Orders.Data.Models;
 using Orders.ViewModels.OrderItem;
 using Orders.ViewModels.Orders;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Orders.Services.OrderServices
@@ -13,37 +17,54 @@ namespace Orders.Services.OrderServices
             _context = context;
         }
 
-        public Task<OrderItemViewModel> AddOrderItemToOrder(OrderItemViewModel orderItem)
+        public async Task<OrderItemViewModel> AddOrderItemToOrder(OrderItemViewModel orderItem)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<OrderViewModel> CreateOrder(OrderViewModel orders)
+        public async Task<OrderViewModel> CreateOrder(OrderViewModel orders)
+        {
+            var result = new Order() { Date = orders.Date, Number = orders.Number, ProviderId = orders.ProviderId };
+            _context.Order.Add(result);
+            await _context.SaveChangesAsync();
+            return orders;
+        }
+
+        public async Task<OrderItemViewModel> CreateOrderItemToOrder(OrderItemViewModel orderItem)
+        {
+            var result = new OrderItem() { Name = orderItem.Name, Quantity = orderItem.Quantity, OrderId = orderItem.OrderId, Unit = orderItem.Unit };
+            _context.OrderItems.Add(result);
+            await _context.SaveChangesAsync();
+            return orderItem;
+        }
+
+        public async Task<IEnumerable<OrderTableViewModel>> GetOrders()
+        {
+            var result = from f in _context.OrderItems.Include(o => o.Order).ThenInclude(p => p.Provider)
+                         select new OrderTableViewModel 
+                         { 
+                             Name = f.Name, 
+                             Quantity = f.Quantity, 
+                             Unit = f.Unit, 
+                             Number = f.Order.Number, 
+                             Date = f.Order.Date, 
+                             ProviderId = f.Order.Provider.Id, 
+                             ProviderName = f.Order.Provider.Name 
+                         };
+            return await result.ToListAsync();
+        }
+
+        public async Task<OrderTableViewModel> GetOrders(int id)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<OrderItemViewModel> CreateOrderItemToOrder(OrderItemViewModel orderItem)
+        public async Task<bool> UpdateOrder(OrderUpdateViewModel orders)
         {
             throw new System.NotImplementedException();
         }
 
-        public Task<OrderTableViewModel> GetOrders()
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<OrderTableViewModel> GetOrders(int id)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> UpdateOrder(OrderUpdateViewModel orders)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Task<bool> UpdateOrderItem(OrderItemUpdateViewModel orderItem)
+        public async Task<bool> UpdateOrderItem(OrderItemUpdateViewModel orderItem)
         {
             throw new System.NotImplementedException();
         }
